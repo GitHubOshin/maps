f8c46c5a4ea5548f6fa25608f38211c6bbee291f
OSHIN <izkp44@gmail.com>
2023-09-28 15:58:34 +0700

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,6 @@
 import { User } from './User'
 import { Company } from './Company'
 
-const user = new User()
+import { CustomMap } from './CustomMap'
 
-console.log(user)
-
-const company = new Company()
-
-console.log(company)
-new google.maps.Map(document.getElementById('map') as HTMLElement, {
-  zoom: 1,
-  center: {
-    lat: 0,
-    lng: 0
-  }
-})
+new CustomMap('map')
